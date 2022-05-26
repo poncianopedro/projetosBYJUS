9a72faa7113c1e02eb6f3a875f725ab2c7cb5680
poncianopedro <102704841+poncianopedro@users.noreply.github.com>
2022-05-26 19:42:36 -0300

    Add files via upload

---
+++ pro-c17-sa-1-template-main/cachorro.js
@@ -0,0 +1,25 @@
+class Cachorro
+{
+    constructor(tamanhoV, racaV, corV, comportamentoV){
+        this.tamanho = tamanhoV;
+        this.raca = racaV;
+        this.cor = corV;
+        this.comportamento = comportamentoV;
+    }
+
+    brincar(){
+
+    }
+
+    comer(){
+
+    }
+
+    beber(){
+
+    }
+
+    pular(){
+        y = 20;
+    }
+}
